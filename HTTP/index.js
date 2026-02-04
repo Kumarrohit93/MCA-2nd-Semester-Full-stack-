fc75481d47816c579f335f45db4e47170f29df46
Rohit Kumar <rk4840369@gmail.com>
2026-02-04 10:17:41 +0530

    update server route

--- HTTP/index.js
+++ HTTP/index.js
@@ -3,10 +3,20 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   if (req.url === "/favicon.ico") return res.end();
-  const log = `${Date.now()} New Request is received from ${req.url}`;
+  const log = `${Date.now()} New Request is received from localhost:3000${req.url}\n`;
   fs.appendFile("log.txt", log, (err, data) => {
     res.end("Hello from server!");
   });
+
+  if(req.url == "/about") {
+    res.end("This is about page")
+  }
+  if(req.url == "/home") {
+    res.end("This is home page")
+  }
+  if(req.url == "/contact") {
+    res.end("This is contact page")
+  }
 });
 
 server.listen(3000, () => {
